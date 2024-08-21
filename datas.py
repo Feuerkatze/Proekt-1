f5759edeeee9cb00fca002ce9532c699cda6a505
Feuerkatze <Artem.2996@yandex.ru>
2024-08-21 18:55:56 +0500

    .08.2024

---
+++ datas.py
@@ -0,0 +1,16 @@
+print(5 // 5)  #  // - целочисленное деление / - деление
+print(5**5)
+print(type(5)) # int - integer- целые цисла -
+print(2.0 + 2) #
+print(type(2.0)) # float - цисло с плавоющей точкой, запятой - дробные числа
+print(' Hello world!')  #  str - String - строка
+print(type('Hello world'))
+print('Hello "world"')
+print('Helo,' + ' world') # concatenate - сложение
+print(5+6+3)
+print(type (True),type(False)) # bool - Boolean - логический тип данных
+print(5>10, 5<10)
+print ( 5 == 5 )
+print(type(int('5')))
+print(5<10, 5>4)
+print(5<10 and 5==5)
