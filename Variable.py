96d67c127b96161e2b85916e964fb4d1fd16adac
Feuerkatze <Artem.2996@yandex.ru>
2024-09-09 22:32:13 +0500

    9.9.24

---
+++ Variable.py
@@ -0,0 +1,5 @@
+home_work = 12
+spent_taim = 1.5
+name = "Python"
+taim_execution = (spent_taim/home_work)
+print("Курс:",name,", всего задач:",home_work,", затрачено часов:",spent_taim,",","среднее время выполнения", taim_execution,"часа.")
