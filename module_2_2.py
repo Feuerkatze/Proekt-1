14ff0714f50512be9e6170771be56836eb3c9a86
Feuerkatze <Artem.2996@yandex.ru>
2024-09-28 00:45:09 +0500

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,11 @@
+first= int(input("Введите 1 число :"))
+second= int(input("Введите 2 число :"))
+third= int(input("Введите 3 число :"))
+print("Из введёных количество равных")
+if first == second== third:
+    print(3," числа")
+elif (first != second== third or first == second!= third or second!= third == first):
+    print(2," числа")
+elif first != second != third:
+    print(0," чисел")
+
