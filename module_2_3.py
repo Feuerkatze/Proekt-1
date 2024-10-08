f4d4a2a98160d545fcaf44f37a5d3a6a52289adc
Feuerkatze <Artem.2996@yandex.ru>
2024-10-08 15:32:41 +0500

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(my_list):
+    if my_list[index] > 0:
+        print(my_list[index])
+        index += 1
+    elif my_list[index] == 0:
+                index += 1
+    elif my_list[index] < 0:#строка не нужная ведь после (отрицательного значения) цыкл не продолжается по условиям выше
+        print("конец")     #Но все равно решил написать
+        break
+
+
