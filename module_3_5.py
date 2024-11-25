085e92efac2df2da0c8d0cec8a53d61fd945bf88
Feuerkatze <Artem.2996@yandex.ru>
2024-11-25 21:34:36 +0500

    Update module_3_5.py

--- module_3_5.py
+++ module_3_5.py
@@ -3,13 +3,18 @@ def get_multiplied_digits(number = 402):
     first= int(str_number[0])
     if 1< int(len(str_number)):
         return first * get_multiplied_digits(int(str_number[1:]))
-    else:
+    elif first != 0:
         return first
+    else:
+        return 1
 
 
 get= get_multiplied_digits(40203)
 print(get)
+
 get1= get_multiplied_digits(70803)
 print(get1)
 get2= get_multiplied_digits()
 print(get2)
+
+print(get_multiplied_digits(4020300000000000))
