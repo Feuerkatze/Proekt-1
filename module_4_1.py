ee9899808b74350e0c80ac5026e3d198c291532b
Feuerkatze <Artem.2996@yandex.ru>
2024-12-04 22:11:24 +0500

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from true_math import divide as Del_W
+from fake_math import divide as Deli
+
+
+result1 = Del_W(69, 3)
+result2 = Del_W(3, 0)
+result3 = Deli(49, 7)
+result4 = Deli(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
